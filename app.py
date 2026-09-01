66472290b6b1be4bfaa67b47654c4f517da60782
didikpurwanto180919-maker <didikpurwanto180919@gmail.com>
2026-09-01 09:23:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
 import streamlit
-import scikit-learn
 import joblib
 import pandas
 import numpy
